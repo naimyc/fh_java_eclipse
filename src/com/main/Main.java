83dd1067d1894ef26ec0331248a8eccb51a77c17
naimyc <naim.yachou-chiva@stud.hs-bochum.de>
2025-01-16 22:12:11 +0100

    Update

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,22 +1,30 @@
 package com.main;
 
+import java.util.Arrays;
+
 import com.classes.Character;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Character z = new Character();
-		char[] a = {'a', 'c', 'h'};
 		
+		char[] cArray = z.eingabe().toCharArray();
 		
-		char zeichen;
-		try {
-			zeichen = (char) z.zeichen(a);
-			System.out.println("Character: " + zeichen);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		if(cArray.length > 3 || cArray.length < 3)
+			throw new Error("Error: Geben Sie nur 3 Zeichen ein!");
 		
+		char a = (char) Math.min(cArray[0], cArray[1]);
+		char e = (char) Math.max(cArray[0], cArray[1]);
+		char g_char = cArray[2];
 		
+		if(g_char < a|| g_char > e)
+			throw new Error("Error: Das dritte Element soll in zwischen den ersten und zweiten sein!");
+		else if((e - a) == 1)
+			throw new Error("Error: Der Bereich ist leer!");
+		
+		int c_value = z.zeichen(cArray);
+
 	}
 }
