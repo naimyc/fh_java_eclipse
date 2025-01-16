a43c91609ffe8ee36bc57bc10e0443665ca11cea
naimyc <naim.yachou-chiva@stud.hs-bochum.de>
2025-01-16 22:12:11 +0100

    Update

--- src/com/classes/Character.java
+++ src/com/classes/Character.java
@@ -1,18 +1,16 @@
 package com.classes;
 
+import javax.swing.JOptionPane;
+
 public class Character {
-	public int zeichen(char[] a) throws Exception {
+	public int zeichen(char[] a) {
 		char anfang = (char) Math.min(a[0], a[1]);
 		char ende = (char) Math.max(a[0], a[1]);
 
 		char g_char = a[2];
 		char rand_char = (char) ((Math.random() * ende - anfang + 1) + ende);
 
-		if (g_char < anfang || g_char > ende) {
-			throw new Exception("Error: Char is not in the interval!");
-		}
-		
-		System.out.println("hola");
+		System.out.println("Run!");
 
 		if (g_char == rand_char) {
 			System.out.println("Found");
@@ -21,5 +19,28 @@ public class Character {
 			return zeichen(a);
 		}
 
+	}
+
+	public String eingabe() {
+		return JOptionPane.showInputDialog("Geben Sie Ihren char Array ein!");
+	}
+
+	public void main(String[] args) {
+		
+		char[] cArray = eingabe().toCharArray();
+
+		if (cArray.length > 3 || cArray.length < 3)
+			throw new Error("Error: Geben Sie nur 3 Zeichen ein!");
+
+		char a = (char) Math.min(cArray[0], cArray[1]);
+		char e = (char) Math.max(cArray[0], cArray[1]);
+		char g_char = cArray[2];
+
+		if (g_char < a || g_char > e)
+			throw new Error("Error: Das dritte Element soll in zwischen den ersten und zweiten sein!");
+		else if ((e - a) == 1)
+			throw new Error("Error: Der Bereich ist leer!");
+
+		int c_value = zeichen(cArray);
 	}
 }
